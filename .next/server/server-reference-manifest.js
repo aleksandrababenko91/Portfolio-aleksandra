160cdc5117c6f57f38e4de83b547f30da247b242
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-05-24 14:27:13 +0300

    added path to git page deploy

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vwALKSyNfdXQuXhr6rWyKBCIE+CYrOSKdPtPWuelcG4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KgStrSJ3y8dTIIdfQoMa9C7myX/IAsk7orFS91xA+RE=\"\n}"
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd3eb7206fb53834"; }
+/******/ 		__webpack_require__.h = function() { return "a8a11c99251a6d73"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
